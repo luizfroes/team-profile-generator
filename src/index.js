591abb82861129759f2a2af1efce5c61e05718b7
Luiz Carlos Froes <luizfroes@gmail.com>
2021-11-23 17:06:27 +0000

    Added the start function structore and sudo code

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const start = () => {
+  let inProgress = true;
+
+  //get team name
+
+  //get manager questions
+
+  while (inProgress) {}
+
+  //build answers object
+
+  //generate HTML
+
+  //Write to file
+};
+
+start();
