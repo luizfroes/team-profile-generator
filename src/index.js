1fe24de50e2be3bbe60836a79bf8c7193bb05a77
Luiz Carlos Froes <luizfroes@gmail.com>
2021-11-24 13:04:45 +0000

    added inquirer questions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
-const start = () => {
+//import inquirer
+const inquirer = require("inquirer");
+
+// import questions
+const {
+  teamQuestion,
+  loopQuestion,
+  managerQuestions,
+  engineerQuestions,
+  internQuestions,
+} = require("./modules/questions");
+
+const start = async () => {
   let inProgress = true;
 
   //get team name
+  const teamName = await inquirer.prompt(teamQuestion);
 
-  //get manager questions
+  while (inProgress) {
+    const role = await inquirer.prompt(loopQuestion);
 
-  while (inProgress) {}
+    if (role === "Manager") {
+      const managerAnswers = await inquirer.prompt(managerQuestions);
+    }
+
+    if (role === "Engineer") {
+      const managerAnswers = await inquirer.prompt(engineerQuestions);
+    }
+
+    if (role === "Intern") {
+      const internAnswers = await inquirer.prompt(internQuestions);
+    }
+
+    if (role === "None") {
+      inProgress = false;
+    }
+  }
 
   //build answers object
 
