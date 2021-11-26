22c31c821b519f2881344b34640b243f20d064d6
Luiz Carlos Froes <luizfroes@gmail.com>
2021-11-26 00:10:31 +0000

    Added intern tests

--- __tests__/intern.test.js
+++ __tests__/intern.test.js
@@ -1,13 +1,39 @@
+const { Intern } = require("../src/lib");
+
 describe("Intern", () => {
-  test("should be an instance of Intern", () => {});
+  const mockIntern = new Intern({
+    school: "University of Birmingham",
+    name: "Jhon Snow",
+    id: "int23",
+    email: "jhonsnow@email.com",
+  });
 
-  test("should return the expected name", () => {});
+  test("should be an instance of Intern", () => {
+    expect(mockIntern).toBeInstanceOf(Intern);
+  });
 
-  test("should return the expected id", () => {});
+  test("should return the expected name", () => {
+    const actual = "Jhon Snow";
+    expect(mockIntern.getName()).toEqual(actual);
+  });
 
-  test("should return the expected email", () => {});
+  test("should return the expected id", () => {
+    const actual = "int23";
+    expect(mockIntern.getId()).toEqual(actual);
+  });
 
-  test("should return the expected school", () => {});
+  test("should return the expected email", () => {
+    const actual = "jhonsnow@email.com";
+    expect(mockIntern.getEmail()).toEqual(actual);
+  });
 
-  test("should return the role", () => {});
+  test("should return the expected school", () => {
+    const actual = "University of Birmingham";
+    expect(mockIntern.getSchool()).toEqual(actual);
+  });
+
+  test("should return the role", () => {
+    const actual = "Intern";
+    expect(mockIntern.getRole()).toEqual(actual);
+  });
 });
