cfa5a6fed5337c1b6716cb5f48eeaefccb69f22c
Luiz Carlos Froes <luizfroes@gmail.com>
2021-11-26 01:00:28 +0000

    added manager tests

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -4,7 +4,7 @@ describe("Engineer", () => {
   const mockEngineer = new Engineer({
     github: "bsmith",
     name: "Bob Smith",
-    id: "man15",
+    id: "eng15",
     email: "bobsmith@email.com",
   });
 
@@ -18,7 +18,7 @@ describe("Engineer", () => {
   });
 
   test("should return the expected id", () => {
-    const actual = "man15";
+    const actual = "eng15";
     expect(mockEngineer.getId()).toEqual(actual);
   });
 
